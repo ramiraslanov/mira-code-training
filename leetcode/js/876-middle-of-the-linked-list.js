f75d35d8c1fa02358a13a7905fbeb720a19adc16
Raslan Amiraslanov <raslan.amiraslanov@gmail.com>
2022-08-25 11:27:50 +0300

    solve(leetcode/js): 876. Middle of The Linked List

--- leetcode/js/876-middle-of-the-linked-list.js
+++ leetcode/js/876-middle-of-the-linked-list.js
@@ -10,10 +10,6 @@
  * @return {ListNode}
  */
 
-function ListNode(val, next) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-}
 var middleNode = function (head) {
     var curr = head;
     var flat = [];
@@ -22,10 +18,6 @@ var middleNode = function (head) {
         curr = curr.next;
     }
 
-    var mid = flat.length / 2;
-    return flat[mid + +(!flat.length % 2)];
+    var mid = Math.floor(flat.length / 2);
+    return flat[mid];
 };
-
-console.log(
-    middleNode(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))))
-);
