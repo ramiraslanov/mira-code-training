bded7b0af962f8e7481978442318c453e95bd8f5
Raslan Amiraslanov <raslan.amiraslanov@gmail.com>
2022-08-28 22:11:59 +0300

    solve(leetcode/js): 5. Longest Palindromic Substring

--- leetcode/js/5-longest-palindromic-substring.js
+++ leetcode/js/5-longest-palindromic-substring.js
@@ -2,6 +2,41 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function (s) {};
 
-console.log(longestPalindrome("abcdedcba"));
+var longestPalindrome = function (s) {
+    var SPECIAL_CHAR = "#";
+    var transformedStr = `${SPECIAL_CHAR}${s.split("").join(SPECIAL_CHAR)}${SPECIAL_CHAR}`;
+    var palindromeRadii = new Array(transformedStr.length);
+    var center = 0;
+    var radius = 0;
+
+    for (var i = 1; i < transformedStr.length - 1; i++) {
+        palindromeRadii[i] = radius > i ? Math.min(radius - i, palindromeRadii[2 * center - i]) : 0;
+
+        while (
+            i - 1 - palindromeRadii[i] >= 0 &&
+            i + 1 + palindromeRadii[i] < transformedStr.length &&
+            transformedStr[i + 1 + palindromeRadii[i]] ===
+                transformedStr[i - 1 - palindromeRadii[i]]
+        ) {
+            palindromeRadii[i] += 1;
+        }
+
+        if (i + palindromeRadii[i] > radius) {
+            center = i;
+            radius = i + palindromeRadii[i];
+        }
+    }
+
+    var maxLen = 0;
+    var centerIndex = -1;
+
+    for (var i = 0; i < palindromeRadii.length; i++) {
+        if (palindromeRadii[i] > maxLen) {
+            maxLen = palindromeRadii[i];
+            centerIndex = i;
+        }
+    }
+
+    return s.slice(Math.floor((centerIndex - maxLen) / 2), Math.floor((centerIndex + maxLen) / 2));
+};
