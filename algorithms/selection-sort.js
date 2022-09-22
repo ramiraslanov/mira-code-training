4b34990557df967db3d4beedbc8a03552d14fd64
Raslan Amiraslanov <raslan.amiraslanov@gmail.com>
2022-09-22 14:55:23 +0300

    feat(algorithms): selection sort

---
+++ algorithms/selection-sort.js
@@ -0,0 +1,18 @@
+const selectionSort = arr => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    return arr;
+};
